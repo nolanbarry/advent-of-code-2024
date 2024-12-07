e6cd3e9196364ba1c322cfe07310bded74f477e7
Nolan Barry <nolan.barry@live.com>
2024-12-06 21:59:51 -0800

    Day 7

---
+++ src/main/kotlin/io/github/nolanbarry/aoc2024/day7/MainA.kt
@@ -0,0 +1,35 @@
+package io.github.nolanbarry.aoc2024.day7
+
+import io.github.nolanbarry.aoc2024.util.Input
+import kotlin.math.PI
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.pow
+import kotlin.math.roundToInt
+import kotlin.math.sin
+
+fun main() {
+    val operators = listOf<(Long, Long) -> Long>(
+        { a, b -> a + b },
+        { a, b -> a * b }
+    )
+    val result = Input.get("day7/input")
+        .lines()
+        .map { it.split(":") }
+        .map { it[0].toLong() to it[1].trim().split(" ").map { it.toLong() } }
+        .filter { (result, numbers) ->
+            buildList { repeat(numbers.size - 1) { add(operators.size) } }
+                .fold(1) { acc, next -> acc * next }
+                .let { 0 until it }
+                .map { it.toString(2).padStart(numbers.size - 1, '0').map { it.toString().toInt() } }
+                .map { whichOperator ->
+                    numbers.reduceIndexed { i, acc, next ->
+                        operators[whichOperator[i - 1]](acc, next)
+                    }
+                }
+                .any { it == result }
+        }
+        .sumOf { (result, _) -> result }
+
+    println(result)
+}
---
+++ src/main/kotlin/io/github/nolanbarry/aoc2024/day7/MainB.kt
@@ -0,0 +1,60 @@
+package io.github.nolanbarry.aoc2024.day7
+
+import io.github.nolanbarry.aoc2024.util.Input
+import kotlin.Long
+import kotlin.collections.any
+import kotlin.collections.buildList
+import kotlin.collections.filter
+import kotlin.collections.fold
+import kotlin.collections.listOf
+import kotlin.collections.map
+import kotlin.collections.reduceIndexed
+import kotlin.collections.sumOf
+import kotlin.io.println
+import kotlin.let
+import kotlin.math.PI
+import kotlin.math.atan2
+import kotlin.math.ceil
+import kotlin.math.cos
+import kotlin.math.log10
+import kotlin.math.pow
+import kotlin.math.roundToInt
+import kotlin.math.sin
+import kotlin.ranges.until
+import kotlin.repeat
+import kotlin.text.lines
+import kotlin.text.map
+import kotlin.text.padStart
+import kotlin.text.split
+import kotlin.text.toInt
+import kotlin.text.toLong
+import kotlin.text.toString
+import kotlin.text.trim
+import kotlin.to
+
+fun main() {
+    val operators = listOf<(Long, Long) -> Long>(
+        { a, b -> a + b },
+        { a, b -> a * b },
+        { a, b -> "$a$b".toLong() }
+    )
+    val result = Input.get("day7/input")
+        .lines()
+        .map { it.split(":") }
+        .map { it[0].toLong() to it[1].trim().split(" ").map { it.toLong() } }
+        .filter { (result, numbers) ->
+            buildList { repeat(numbers.size - 1) { add(operators.size) } }
+                .fold(1) { acc, next -> acc * next }
+                .let { 0 until it }
+                .map { it.toString(operators.size).padStart(numbers.size - 1, '0').map { it.toString().toInt() } }
+                .map { whichOperator ->
+                    numbers.reduceIndexed { i, acc, next ->
+                        operators[whichOperator[i - 1]](acc, next)
+                    }
+                }
+                .any { it == result }
+        }
+        .sumOf { (result, _) -> result }
+
+    println(result)
+}
